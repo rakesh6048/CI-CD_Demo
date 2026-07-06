938723bf466e020c5fbef8d8d66951e3b6e00f83
rakesh <rakesh.s6048@gmail.com>
2026-07-06 19:33:41 +0530

    Changes Done

--- src/test/java/Test/PreAndPostIncrerment.java
+++ src/test/java/Test/PreAndPostIncrerment.java
@@ -6,49 +6,71 @@ public class PreAndPostIncrerment {
 
 	@Test
 	public void postIncrement() {
-		
+
 		int a=10;
 		int b=a++;
-		
+
 		System.out.println("PostIncrement Result");  
-		
+
 		System.out.println("a = "+a);
-		System.out.println("b = "+b);
-		
-		System.out.println("a = "+a);
-		System.out.println("b = "+b);
-		
-		System.out.println("a = "+a);
+		System.out.println("b = "+b);		
+
+	}
+
+	@Test                                
+	public void preIncrement() {      
+
+		int a=10;                      
+		int b=++a; 
+
+		System.out.println("PreIncrement Result");  
+
+		System.out.println("a = "+a);  
 		System.out.println("b = "+b);
 	}
-		
-        @Test                                
-public void preIncrement() {      
-		                               
-	int a=10;                      
-	int b=++a; 
-	
-	System.out.println("PreIncrement Result");  
-	                               
-	System.out.println("a = "+a);  
-	System.out.println("b = "+b);
-        }
-        
-        @Test
-        public void tstContinue() {
-        	 int x = 10;
-        
-       System.out.println("Continue Statement");
-       System.out.println("");
-             do {
-                x++;
-                if(x == 15){
-                   continue;		 
-                }   
-                System.out.print("value of x : " + x );
-                System.out.print("\n");
-                System.out.print("\n");
-             } while( x < 20 );
-        }
+
+	@Test
+	public void tstContinue() {
+		int x = 10;
+
+		System.out.println("Continue Statement");
+		System.out.println("");
+		do {
+			x++;
+			if(x == 15){
+				continue;		 
+			}   
+			System.out.print("value of x : " + x );
+			System.out.print("\n");
+		} while( x < 20 );
+	}
+
+
+	@Test                                                                           
+	public void preIncrement1() {                                                            
+
+		int a=10;                                                                           
+		int b=++a;                                                                          
+
+		System.out.println("PreIncrement Result");                                          
+
+		System.out.println("a = "+a);                                                       
+		System.out.println("b = "+b);                                                       
+	}                                                                               
+
+	@Test                                                                           
+	public void tstContinue1() {                                                     
+		int x = 10;                                                                
+
+		System.out.println("Continue Statement");                                        
+		System.out.println("");                                                          
+		do {                                                                       
+			x++;                                                                    
+			if(x == 15){                                                            
+				continue;		                                                    
+			}                                                                       
+			System.out.print("value of x : " + x );                                 
+			System.out.print("\n");                                                 
+		} while( x < 20 );                                                         
+	}                                                                               
 }                                  
-	                               
