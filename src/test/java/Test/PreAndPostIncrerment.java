c0776a72121545c4cea49b324a9a38bebe2b7ebe
rakesh6048 <68732430+rakesh6048@users.noreply.github.com>
2026-08-20 18:38:35 +0530

    Fix duplicate print statement for variable 'b'

--- src/test/java/Test/PreAndPostIncrerment.java
+++ src/test/java/Test/PreAndPostIncrerment.java
@@ -14,6 +14,7 @@ public class PreAndPostIncrerment {
 
 		System.out.println("a = "+a);
 		System.out.println("b = "+b);
+			System.out.println("b = "+b);
 		
 		System.out.println("a = "+a);
 		System.out.println("a = "+a);
